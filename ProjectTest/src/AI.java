696c82dab740c017db980901ca7d22936958a473
ros_erlin <ros_erlin@WROSRM31921.seattleschools.org>
2014-09-19 08:45:43 -0700

    1st commit

---
+++ ProjectTest/src/AI.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+
+public class AI {
+	private ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>(0);
+	private int total;
+	private int num;
+	
+	public AI(int total, int num){
+		for(int x=1; x<=total; x++){
+			ArrayList<Integer> nums = new ArrayList<Integer>(0);
+			for(int y=1; y<=num; y++){
+				nums.add(y);
+			}
+			this.list.add(nums);
+		}
+		this.total = total;
+		this.num = num;
+	}
+	
+	public int chooseNum(int remain){
+		int chosen = list.get(remain).get((int)(Math.random()*num));
+		return chosen;
+	}
+	
+	
+	
+}
---
+++ ProjectTest/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+	}
+	
+	public static void startGame(){
+		
+	}
+	
+	public static void game1(){
+		
+	}
+	
+	public static void game2(){
+		
+	}
+	
+	public static void game3(){
+		
+	}
+	
+	public static void game4(){
+		
+	}
+	
+	public static AI getAI(int total, int num){
+		AI AI = new AI(total, num);
+		return AI;
+	}
+}
