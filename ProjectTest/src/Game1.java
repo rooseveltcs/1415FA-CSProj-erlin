b2db04fc47098c1cdc8bb548cea7f25078a44de5
ros_erlin <ros_erlin@WROSRM31921.seattleschools.org>
2014-09-23 09:05:30 -0700

    2nd commit

--- ProjectTest/src/Game1.java
+++ ProjectTest/src/Game1.java
@@ -11,13 +11,19 @@ public class Game1 {
 	}
 	
 	public void gameStart(){
-		
+		while(total>0){
+			System.out.println("Player1: How many do you choose?(1-5)");
+			String str1 = Main.console.next();
+			inputError(str1,1,5);
+			//resume here
+		}
 	}
 	
 	public void numError(String temp, int min, int max){
 		if(Main.isInteger(temp)){
 			int num = Integer.parseInt(temp);
 			if(num>=min && num<=max){
+				total = num;
 				gameStart();
 			}
 			else{
@@ -31,6 +37,23 @@ public class Game1 {
 		}
 		else{
 			Main.repeat = false;
+		}
+	}
+	
+	public void inputError(String temp, int min, int max){
+		if(Main.isInteger(temp)){
+			int num = Integer.parseInt(temp);
+			if(num>=min && num<=max){
+				total -= num;
+			}
+			else{
+				System.out.println("error: input int not within range of "+ min + " and " + max);
+				inputError(Main.console.next(),min,max);
+			}
+		}
+		else{
+			System.out.println("error: input not an int");
+			inputError(Main.console.next(),min,max);
 		}
 	}
 }
