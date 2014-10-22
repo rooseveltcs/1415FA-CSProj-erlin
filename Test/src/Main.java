4d2b3250a7b3cf8b159cfe3fc94398aed419d0e5
ros_erlin <ros_erlin@WROSRM31921.seattleschools.org>
2014-10-22 08:44:39 -0700

    a

---
+++ Test/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+	
+}
+
+/*
+ * 108 cards
+ * 4-colors
+ * (1-9) x2 each color
+ * (0) x1 each color
+ * (reverse, +2, skip) x2 each color
+ * (wild, wild+4) x4
+ * 
+ * 7 cards to each player
+ * moves possible:
+ * 	play card, matching symbol/color
+ * 	play wild/wild+4(only if no other same color cards)
+ * 	draw card from deck if no playable cards, can play if drawn card playable
+ * 
+ * Rules:
+ * 	skip:		skips next person, or first person
+ *	draw2:		next player +2, or first person
+ *	reverse:	switches order of play
+ *	wild:		player chooses color, or first person
+ *	wildDraw4:	declare color and next person +4, re-shuffle and put new card
+ */	
