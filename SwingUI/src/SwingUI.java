a88521b4a101850ca2d33702b46a58d29ec8e0d6
ros_erlin <ros_erlin@WROSRM31921.seattleschools.org>
2014-10-31 08:43:48 -0700

    1st commit

--- SwingUI/src/SwingUI.java
+++ SwingUI/src/SwingUI.java
@@ -11,34 +11,48 @@ public class SwingUI extends JFrame {
 	  JTextField numberField1 = new JTextField(10);
 	  JTextField numberField2 = new JTextField(10);
 	  JTextField resultField = new JTextField(20);
-
+	  
+	  JButton butt1 = new JButton("Product of n1 and n2");
+	  
 	  public static void main(String[] args) {
-		  SwingUI app = new SwingUI();
+		SwingUI app = new SwingUI();
 	    app.setVisible(true);
 	  }
 
 	  private SwingUI() {
-	    super("Product of numbers");
+	    super("Calculator");
 	    resultField.setEditable(false);
 	    add(new JLabel("n1 times n2"), BorderLayout.NORTH);
 	    add(numberField1, BorderLayout.CENTER);
 	    add(numberField2, BorderLayout.WEST);
-	    add(resultField, BorderLayout.PAGE_END);
-	    JButton butt = new JButton("Product of n1 and n2");
-	    butt.addActionListener(new ActionListener() {
+	    add(butt1, BorderLayout.EAST);
+	    add(resultField, BorderLayout.AFTER_LAST_LINE);
+	    butt1.addActionListener(new ActionListener() {
 	      public void actionPerformed(ActionEvent e) {
 	        String numberStr1 = numberField1.getText();
 	        String numberStr2 = numberField2.getText();
-	        numberStr1 = numberStr1.trim();
-	        numberStr2 = numberStr2.trim();
-	        int n1 = Integer.parseInt(numberStr1);
-	        int n2 = Integer.parseInt(numberStr2);
-	        n1 *= n2;
-	        resultField.setText("num1 * num2 = " + n1);
+	        if(isInteger(numberStr1) && isInteger(numberStr2)){
+	        	int n1 = Integer.parseInt(numberStr1);
+	        	int n2 = Integer.parseInt(numberStr2);
+	        	n1 *= n2;
+	        	resultField.setText("num1 + num2 = " + n1);
+	        }
+	        else{
+	        	resultField.setText("error");
+	        }
 	      }
 	    });
-	    add(butt, BorderLayout.EAST);
 	    setDefaultCloseOperation(EXIT_ON_CLOSE);
 	    pack();
 	  }
+	  
+	  public boolean isInteger( String input ) {
+		    try {
+		        Integer.parseInt( input );
+		        return true;
+		    }
+		    catch( Exception e ) {
+		        return false;
+		    }
+		}
 	}
