3cda0597924f277fdda9adc75716f26aa429084e
ros_erlin <ros_erlin@WROSRM31921.seattleschools.org>
2014-11-04 08:30:53 -0800

    2nd commit 	changed stuff 	Made buttonFrame unclosable

--- ProjectTestv4/src/ButtonsFrame.java
+++ ProjectTestv4/src/ButtonsFrame.java
@@ -1,8 +1,11 @@
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,7 +18,8 @@ public class ButtonsFrame  extends JFrame{
 	public ButtonsFrame() {
 		createGui();
 	}
-	public void createGui() {
+	public void createGui() throws HeadlessException{
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		//things/varaibles...etc
 		this.setTitle("Buttons");
 		this.setResizable(false);
@@ -161,18 +165,16 @@ public class ButtonsFrame  extends JFrame{
 		contentPane.add(buttonC, bC);
 	}
 	
-	/*
 	//testing to open frame
 	public static void main(String[] args) {
 		ButtonsFrame frame = new ButtonsFrame();
 		frame.pack();
+		frame.setDefaultCloseOperation(frame.DO_NOTHING_ON_CLOSE);
 		frame.addWindowListener(new WindowAdapter() {
-
 			public void windowClosing(WindowEvent e) {
-				System.exit(0);
+				//System.exit(0);
 			}
 		});
 		frame.setVisible(true);
 	}
-*/
 }
